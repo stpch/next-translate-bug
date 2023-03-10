45d83d4cfd4ecf31a93b00f1d97e1b790dea2b44
Stéphane Chorne <stephane.chorne@gmail.com>
2023-03-10 15:23:44 +0100

    Set up minimal Next.js project

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { FC } from 'react'
+
+const HomePage: FC = () => {
+    return (
+        <>
+            <Head>
+                <title>next-translate bug example</title>
+            </Head>
+            <main>
+                TODO: Example
+            </main>
+        </>
+    )
+}
+
+export default HomePage
